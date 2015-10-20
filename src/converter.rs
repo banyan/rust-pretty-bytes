7a0f4f77b51339b06239fb82185fdbcaea0ed961
Kohei Hasegawa <ameutau@gmail.com>
2015-10-21 08:57:49 +0900

    Use num

--- src/converter.rs
+++ src/converter.rs
@@ -1,24 +1,26 @@
+use num::pow;
 use std::cmp;
 
-pub fn convert(num: f64) -> () {
-  println!("{:?}", num);
-
+// TODO use Bigint for 10^24
+pub fn convert(number: f64) -> () {
   // TODO Use is_sign_positive
   // https://doc.rust-lang.org/std/primitive.f64.html?search=is_sign_positive
-  // let negative = num < 0;
+  // let negative = number < 0;
 
   // TODO check unit size
   let units = ["B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
 
   // TODO fix variable name later
   let x = 1000_f64;
-  let a = (num.ln() / x.ln());
+  let a = (number.ln() / x.ln());
   let exponent = a.floor();
 
   // https://doc.rust-lang.org/std/primitive.f64.html#method.exp2
   // exp2 can be used
-  // num = (num / x.powf(exponent)) * 1;
+
+  let z = pow(1000, exponent as usize);
 
   // TODO return generated strings
-  println!("{:?}", num);
+  println!("{:?}", exponent);
+  println!("{:?}", pow(1000, 3));
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 extern crate getopts;
+extern crate num;
 
 pub mod cli;
 pub mod converter;
