7f5ede5ca1617be67ec0b4bd249814766a97b9ae
Kohei Hasegawa <ameutau@gmail.com>
2015-10-26 08:47:06 +0900

    Add tests

--- src/converter.rs
+++ src/converter.rs
@@ -5,9 +5,12 @@ pub fn convert(num: f64) -> String {
   // TODO check unit size
   let units = vec!["B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
 
+  if num < 1_f64 {
+      return format!("{}{} {}", negative, num, "B");
+  }
   let delimiter = 1000_f64;
   let exponent = (num.ln() / delimiter.ln()).floor() as i32;
-  let pretty_bytes = num / delimiter.powi(exponent);
+  let pretty_bytes = format!("{:.2}", num / delimiter.powi(exponent)).parse::<f64>().unwrap() * 1_f64;
   let unit = units[exponent as usize];
-  format!("{}{:.2} {}", negative, pretty_bytes, unit)
+  format!("{}{} {}", negative, pretty_bytes, unit)
 }
